8acc514c392e270412f9256b2b5bdfcb6a6bfcef
anna <sudolann@gmail.com>
2021-11-15 15:03:59 +0000

    header updates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -105,6 +105,29 @@ export default function Header() {
     </Menu>
   );
 
+  const menuItems = (
+    <>
+      <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+        <IconButton size='large' aria-label='show 4 new mails' color='inherit'>
+          <AccountBalanceWalletIcon />
+        </IconButton>
+        <IconButton size='large' aria-label='show 17 new notifications' color='inherit'>
+          <Badge badgeContent={17} color='error'>
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <IconButton size='large' edge='end' aria-label='account of current user' aria-controls={menuId} aria-haspopup='true' onClick={handleProfileMenuOpen} color='inherit'>
+          <AccountCircle />
+        </IconButton>
+      </Box>
+      <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+        <IconButton size='large' aria-label='show more' aria-controls={mobileMenuId} aria-haspopup='true' onClick={handleMobileMenuOpen} color='inherit'>
+          <MoreIcon />
+        </IconButton>
+      </Box>
+    </>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
@@ -112,24 +135,7 @@ export default function Header() {
           <Logo />
           <SearchUI />
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size='large' aria-label='show 4 new mails' color='inherit'>
-              <AccountBalanceWalletIcon />
-            </IconButton>
-            <IconButton size='large' aria-label='show 17 new notifications' color='inherit'>
-              <Badge badgeContent={17} color='error'>
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton size='large' edge='end' aria-label='account of current user' aria-controls={menuId} aria-haspopup='true' onClick={handleProfileMenuOpen} color='inherit'>
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton size='large' aria-label='show more' aria-controls={mobileMenuId} aria-haspopup='true' onClick={handleMobileMenuOpen} color='inherit'>
-              <MoreIcon />
-            </IconButton>
-          </Box>
+          {menuItems}
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
