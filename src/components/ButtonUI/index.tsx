41c6fd99c363a47183f08a66d586b5ddebbd9735
anna <sudolann@gmail.com>
2021-11-15 16:17:09 +0000

    buttons updated

--- src/components/ButtonUI/index.tsx
+++ src/components/ButtonUI/index.tsx
@@ -22,13 +22,13 @@ export interface ButtonProps {
 const ButtonUI: React.FunctionComponent<ButtonProps> = ({ onClick, children, type = ButtonUIType.button, disabled = false, variant = VariantType.contained, color = 'main-100' }) => {
   const colorNumber: number = parseFloat(color.split('-')[1] || '100');
   const hoverColor = `${color.split('-')[0]}-${colorNumber + 100}`;
-  console.log(hoverColor, colorNumber, 'hoverColor');
+  console.log(color, hoverColor, colorNumber, 'hoverColor');
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={variant === VariantType.contained ? `bg-${color} hover:bg-${hoverColor} text-white font-bold py-2 px-4 rounded` : `bg-transparent hover:bg-${color}-200 text-main-700 font-semibold hover:text-white py-2 px-4 border border-main-500 rounded`}
+      className={variant === VariantType.contained ? `bg-${color} hover:bg-${hoverColor} text-white font-bold py-2 px-4 rounded` : `bg-transparent hover:bg-${color}-200 text-main-700 font-semibold hover:text-white py-2 px-4 border border-main-500 rounded mx-2`}
     >
       {children}
     </button>
