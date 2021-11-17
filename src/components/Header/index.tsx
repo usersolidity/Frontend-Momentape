d471258c56537929594c187149328632c45e0bf6
anna <sudolann@gmail.com>
2021-11-17 16:58:37 +0000

    edited path name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -62,7 +62,7 @@ export default function PrimarySearchAppBar() {
         <p>Notifications</p>
       </MenuItem>
       <MenuItem>
-        <Link href='/profile'>
+        <Link href='/123'>
           <IconButton size='large' edge='end' aria-label='account of current user' aria-controls={menuId} aria-haspopup='true' color='inherit'>
             <AccountCircle />
           </IconButton>
@@ -88,7 +88,7 @@ export default function PrimarySearchAppBar() {
                 <NotificationsIcon />
               </Badge>
             </IconButton>
-            <Link href='/profile'>
+            <Link href='/123'>
               <IconButton size='large' edge='end' aria-label='account of current user' aria-controls={menuId} aria-haspopup='true' color='inherit'>
                 <AccountCircle />
               </IconButton>
