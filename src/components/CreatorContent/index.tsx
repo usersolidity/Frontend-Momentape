f7663c4de4a9365464978648aa144b1b3fa69c33
anna <sudolann@gmail.com>
2021-11-17 18:50:58 +0000

    profile section edited

--- src/components/CreatorContent/index.tsx
+++ src/components/CreatorContent/index.tsx
@@ -1,11 +1,37 @@
 import styles from './CreatorContent.module.css';
+import _ from 'lodash';
 
 type ICreatorContentProps = {
-  imgPath: string;
+  imgPath: {
+    profile: string;
+    cover: string;
+  };
+  name: string;
+  ethAddress: string;
 };
 
-const CreatorContent: React.FunctionComponent<ICreatorContentProps> = ({ imgPath }) => {
-  return <div className={styles.cover} style={{ backgroundImage: `url(${imgPath})` }}></div>;
+const CreatorContent: React.FunctionComponent<ICreatorContentProps> = ({ imgPath, name, ethAddress }) => {
+  return (
+    <div>
+      <div className={styles.cover} style={{ backgroundImage: `url(${imgPath.cover})` }}></div>
+      <div className='container'>
+        <div className='flex'>
+          <div className='w-24 h-24 rounded relative bottom-12'>
+            <img src={imgPath.profile} alt='profile img' className='w-24 h-24 rounded object-cover' />
+          </div>
+          <div>
+            <span className='text-gray-900 text-2xl font-bold ml-2 block'>{name}</span>
+            <span className='text-gray-800 block ml-2'>
+              {_.truncate(ethAddress, {
+                length: 14,
+                separator: ' ',
+              })}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatorContent;
--- src/pages/[creatorId].tsx
+++ src/pages/[creatorId].tsx
@@ -4,9 +4,12 @@ import { CreatorContent } from '../components';
 const Creator = () => {
   const router = useRouter();
   const { creatorId } = router.query;
+  const profileIMG = creatorId ? `${router.basePath}/assets/images/${creatorId}-profile.jpg` : `${router.basePath}/assets/images/user.png`;
+  const name = 'Anna Sudol';
+  const ethAddress = '0xFE92A2bbA39CdF36b53Cab3C8e6cC61bE9710eF6';
   return (
     <Base>
-      <CreatorContent imgPath={`${router.basePath}/assets/images/${creatorId}.jpg`} />
+      <CreatorContent imgPath={{ cover: `${router.basePath}/assets/images/${creatorId}.jpg`, profile: profileIMG }} name={name} ethAddress={ethAddress} info={{ followers: 11222, following: 32111 }} />
     </Base>
   );
 };
