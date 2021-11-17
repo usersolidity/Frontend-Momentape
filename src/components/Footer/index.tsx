eef20c13ed846ad39915a0d5ba336ca05803559b
anna <sudolann@gmail.com>
2021-11-17 15:21:19 +0000

    updates

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,9 @@
-import Link from 'next/link';
-
 import { Logo } from '../../components';
 import { FooterCopyright } from './FooterCopyright';
 
 const Footer = () => (
-  <div className='bg-gray-100 flex flex-col items-center justify-center p-8'>
+  <div className='bg-gray-100 flex flex-col items-center justify-center pt-2 pb-4 absolute bottom-0 left-0 right-0 mt-64'>
     <Logo classes='text-main-100' />
-
     <div className='mt-8 text-sm'>
       <FooterCopyright />
     </div>
--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,12 +1,11 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { Header, Footer } from '../components';
+import { Header } from '../components';
 const Base: React.FunctionComponent = ({ children }) => (
-  <div className='antialiased text-gray-600'>
+  <div className='relative'>
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Header />
     {children}
-    <Footer />
   </div>
 );
 
