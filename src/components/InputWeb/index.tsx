4c677c482b174c9c5225e61b60bbdc8ecf83e9f4
nhh <nijat.hasanli.h@gmail.com>
2021-11-19 20:16:58 +0000

    Added a name to the component

--- src/components/InputWeb/index.tsx
+++ src/components/InputWeb/index.tsx
@@ -1,29 +1,40 @@
-import * as React from 'react';
+import * as React from "react";
 export interface IInputProps {
-  onChange: any;
-  value: any;
-  label: string;
+    onChange: any;
+    value: any;
+    name: string;
+    label: string;
 }
-const Input: React.FunctionComponent<IInputProps> = ({ onChange, value, label }) => {
-  return (
-    <div className='col-span-3 sm:col-span-2 mt-2'>
-      <label htmlFor='company-website' className='block text-sm font-medium text-gray-700'>
-        {label}
-      </label>
-      <div className='mt-1 flex rounded-md shadow-sm'>
-        <span className='inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm'>http://</span>
-        <input
-          onChange={(e: any) => onChange(e.target.value)}
-          value={value}
-          type='text'
-          name='company-website'
-          id='company-website'
-          className='focus:ring-main-100 focus:border-main-100 flex-1 block w-full rounded-none rounded-r-md sm:text-sm  border border-gray-300 p-3'
-          placeholder='www.example.com'
-        />
-      </div>
-    </div>
-  );
+const Input: React.FunctionComponent<IInputProps> = ({
+    onChange,
+    value,
+    name,
+    label,
+}) => {
+    return (
+        <div className="col-span-3 sm:col-span-2 mt-2">
+            <label
+                htmlFor="company-website"
+                className="block text-sm font-medium text-gray-700"
+            >
+                {label}
+            </label>
+            <div className="mt-1 flex rounded-md shadow-sm">
+                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                    http://
+                </span>
+                <input
+                    onChange={(e: any) => onChange(e)}
+                    value={value}
+                    type="text"
+                    name={name}
+                    id="company-website"
+                    className="focus:ring-main-100 focus:border-main-100 flex-1 block w-full rounded-none rounded-r-md sm:text-sm  border border-gray-300 p-3"
+                    placeholder="www.example.com"
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Input;
