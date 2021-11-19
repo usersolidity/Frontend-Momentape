02f1a36e2911c7a4b336875938fde832c94d38cf
nhh <nijat.hasanli.h@gmail.com>
2021-11-19 19:21:16 +0000

    Update InputBase component
    * Linted file
    * Added optional disable attr
    * Default value is value passed to function

--- src/components/InputBase/index.tsx
+++ src/components/InputBase/index.tsx
@@ -1,27 +1,57 @@
-import * as React from 'react';
+import * as React from "react";
 
 enum InputType {
-  text = 'text',
-  number = 'number',
+    text = "text",
+    number = "number",
 }
 
 export interface IInputProps {
-  onChange: any;
-  type?: InputType;
-  value: any;
-  label: string;
-  textArea?: boolean;
+    onChange: any;
+    type?: InputType;
+    value: any;
+    label: string;
+    textArea?: boolean;
+    disabled?: boolean;
 }
-const InputBase: React.FunctionComponent<IInputProps> = ({ onChange, type = InputType.text, value, label, textArea = false }) => {
-  const classes = 'mt-1 p-2 focus:ring-main-100 focus:border-main-100 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md';
-  return (
-    <div className='col-span-6 sm:col-span-3 mt-2'>
-      <label htmlFor={label} className='block text-sm font-medium text-gray-700'>
-        {label}
-      </label>
-      {textArea ? <textarea name={label} id={label} rows={3} className={classes} defaultValue={''} /> : <input type={type} name={label} id={label} value={value} onChange={(e: any) => onChange(e.target.value)} className={classes} />}
-    </div>
-  );
+const InputBase: React.FunctionComponent<IInputProps> = ({
+    onChange,
+    type = InputType.text,
+    value,
+    label,
+    textArea = false,
+    disabled = false,
+}) => {
+    const classes =
+        "mt-1 p-2 focus:ring-main-100 focus:border-main-100 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md";
+    return (
+        <div className="col-span-6 sm:col-span-3 mt-2">
+            <label
+                htmlFor={label}
+                className="block text-sm font-medium text-gray-700"
+            >
+                {label}
+            </label>
+            {textArea ? (
+                <textarea
+                    name={label}
+                    id={label}
+                    rows={3}
+                    className={classes}
+                    defaultValue={value}
+                />
+            ) : (
+                <input
+                    type={type}
+                    name={label}
+                    id={label}
+                    value={value}
+                    onChange={(e: any) => onChange(e.target.value)}
+                    className={classes}
+                    disabled={disabled}
+                />
+            )}
+        </div>
+    );
 };
 
 export default InputBase;
