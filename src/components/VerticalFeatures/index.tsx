d3edaeef63f4aed9741cf6f157b9c699798d4c87
Luca Perret <perret.luca@gmail.com>
2021-11-19 17:20:21 +0100

    feat(home): CTAs

--- src/components/VerticalFeatures/index.tsx
+++ src/components/VerticalFeatures/index.tsx
@@ -1,29 +1,81 @@
-import { VerticalFeatureRow } from './VerticalFeatureRow';
-import { ButtonUI } from '../../components';
-import { Section } from '../../layout/Section';
-import { VariantType } from '../ButtonUI/index';
+import { useRouter } from "next/router";
 
-const VerticalFeatures = () => (
-  <Section>
-    <VerticalFeatureRow
-      title='Collect creators&#39; Top Moments'
-      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.'
-      image={{ path: 'feature.svg', alt: 'feature' }}
-    >
-      <ButtonUI variant={VariantType.contained}>Get started as an artist</ButtonUI>
-    </VerticalFeatureRow>
-    <VerticalFeatureRow
-      title='You’re an Artist'
-      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.'
-      image={{ path: 'feature.svg', alt: 'feature' }}
-      reverse
-    >
-      <ButtonUI variant={VariantType.outlinedMain}>Get started as an artist</ButtonUI>
-    </VerticalFeatureRow>
-    <VerticalFeatureRow title='You’re a Collector' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.' image={{ path: 'feature.svg', alt: 'feature' }}>
-      <ButtonUI variant={VariantType.contained}>Start collecting</ButtonUI>
-    </VerticalFeatureRow>
-  </Section>
-);
+import modelAliases from "../../data/model.json";
+
+import { VerticalFeatureRow } from "./VerticalFeatureRow";
+import { ButtonUI } from "../../components";
+import { Section } from "../../layout/Section";
+import { VariantType } from "../ButtonUI/index";
+
+const VerticalFeatures = () => {
+    const router = useRouter();
+
+    async function authenticate() {
+        const [address] = await (window as any).ethereum.enable();
+        const { EthereumAuthProvider, WebClient } = await import(
+            "@self.id/web"
+        );
+        const client = new WebClient({
+            ceramic: "testnet-clay",
+            model: modelAliases,
+        });
+        if (address) {
+            const ethAuthProvider = new EthereumAuthProvider(
+                (window as any).ethereum,
+                address
+            );
+
+            const did = await client.authenticate(ethAuthProvider, true);
+            if (did.id) {
+                router.push({
+                    pathname: "/[creatorId]",
+                    query: { creatorId: did.id.replace("did:3:", "") },
+                });
+            } else {
+                router.push({
+                    pathname: "/newCreator",
+                });
+            }
+        }
+    }
+    return (
+        <Section>
+            <VerticalFeatureRow
+                title="Collect creators&#39; Top Moments"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+                image={{ path: "feature.svg", alt: "feature" }}
+            />
+            <VerticalFeatureRow
+                title="You’re an Artist"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+                image={{ path: "feature.svg", alt: "feature" }}
+                reverse
+            >
+                <ButtonUI
+                    variant={VariantType.contained}
+                    onClick={() =>
+                        router.push({
+                            pathname: "/newCreator",
+                        })
+                    }
+                >
+                    Get started as an artist
+                </ButtonUI>
+                <br />
+                <ButtonUI
+                    variant={VariantType.contained}
+                    onClick={authenticate}
+                >
+                    Connect to see your profile
+                </ButtonUI>
+            </VerticalFeatureRow>
+            <VerticalFeatureRow
+                title="You’re a Collector"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+                image={{ path: "feature.svg", alt: "feature" }}
+            />
+        </Section>
+    );
+};
 
 export default VerticalFeatures;
